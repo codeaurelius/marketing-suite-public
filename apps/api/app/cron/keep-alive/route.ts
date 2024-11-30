faaf07fb155123407a786fb2a1d0eb19f784c1a3
codeaurelius <stoic1984@icloud.com>
2024-11-30 17:57:45 +0200

    chore: fix keep-alive route

--- apps/api/app/cron/keep-alive/route.ts
+++ apps/api/app/cron/keep-alive/route.ts
@@ -1,18 +1,18 @@
-import { database } from '@repo/database';
+// import { database } from '@repo/database';
 
 export const POST = async () => {
-  const newPage = await database.page.create({
-    data: {
-      name: 'cron-temp',
-      email: 'test@test.com',
-    },
-  });
+  // const newPage = await database.page.create({
+  //   data: {
+  //     name: 'cron-temp',
+  //     email: 'test@test.com',
+  //   },
+  // });
 
-  await database.page.delete({
-    where: {
-      id: newPage.id,
-    },
-  });
+  // await database.page.delete({
+  //   where: {
+  //     id: newPage.id,
+  //   },
+  // });
 
   return new Response('OK', { status: 200 });
 };
